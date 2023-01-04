e27fc33c1d2a9f56883983ae5a9d4ca32f93c4f9
joelsiberia <35606477+joelleeyueer@users.noreply.github.com>
2023-01-04 19:01:20 +0800

    Added shuffleDeck method

--- src/sdr_cards/cardGameMain.java
+++ src/sdr_cards/cardGameMain.java
@@ -1,13 +1,15 @@
 package src.sdr_cards;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class cardGameMain {
 
+    static ArrayList<Cards> decklist = new ArrayList<>();
+
     public static void main(String[] args) {
         Constants cons = new Constants();
-        ArrayList<Cards> decklist = new ArrayList<>();
-
         //generate card for each suit and rank
         for (int suitIndex = 0; suitIndex<cons.SUIT.length; suitIndex++){
             for (int rankIndex = 0; rankIndex<cons.RANK.length; rankIndex++){
@@ -17,6 +19,20 @@ public class cardGameMain {
         }
 
         //print decklist
-        System.out.println(decklist);
+        System.out.println("Printing the unshuffled decklist:\n" + decklist);
+        shuffleDeck();
+    }
+
+    public static void shuffleDeck(){
+        Random random = new SecureRandom();
+        //iterate through array and swap current element with a random element in the array
+        for (int currIndex = 0; currIndex<decklist.size(); currIndex++){
+            Cards currentCard = decklist.get(currIndex);
+            int randomIndex = random.nextInt(decklist.size());
+            Cards toSwapCard = decklist.get(randomIndex);
+            decklist.set(currIndex,toSwapCard);
+            decklist.set(randomIndex,currentCard);
+        }
+        System.out.println("Printing the shuffled decklist:\n" + decklist);
     }
 }
